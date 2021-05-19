810a9fc52b2fb8acfe6c852a34f1f5c8fb766d7a
DESKTOP-J50OD4I\admin <trungtan.nguyen@team.enouvo.com>
2021-05-19 19:08:43 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from "./Todo";
 
 function App() {
+  const [state, setState] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setState(e.target.value);
+  }
+
+  const handleClick = () => {
+    const newTodo = todos;
+    newTodo.push(state);
+    setTodos(newTodo);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={state} onChange={(e) => handleChange} />
+      <button onClick={handleClick}>Submit</button>
+      {todos.map(e => <Todo todo={e} />)}
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todo = ({ todo }) => {
+    return (
+        <div className="Todo">
+            {todo}
+        </div>
+    );
+}
+
+export default Todo;
